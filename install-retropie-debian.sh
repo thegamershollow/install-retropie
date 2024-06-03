243f21e80253b26965de3d380ecdf27fa9b601ea
Ethan <125901017+thegamershollow@users.noreply.github.com>
2024-06-03 21:31:26 +0000

    Update install-retropie-debian.sh

--- install-retropie-debian.sh
+++ install-retropie-debian.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade
-sudo apt install -y git dialog unzip xmlstartlet tmux neofetch htop gparted flatpak curl
+sudo apt install -y git dialog unzip xmlstartlet tmux neofetch htop gparted flatpak curl blueman
 git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
 cd RetroPie-Setup
 sudo ./retropie_setup.sh
